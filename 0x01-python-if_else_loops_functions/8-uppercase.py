e30d02a5c1e5abdf0b67974ccc93ee8c359d291f
mouad el atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-09-05 10:25:06 -0700

    upercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,8 @@ def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
+            string += i
+        else:
+            string += i
+    print("{:s}".format(string), end="")
     print()
