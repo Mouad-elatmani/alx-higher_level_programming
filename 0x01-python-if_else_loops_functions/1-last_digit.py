92d9549c768da558cf25e69bc72513c444eb2958
mouad el atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-09-05 06:58:43 -0700

    last digit update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 if number > 0:
     n = number % 10
-else :
-    number*= -1
-    n = (number %10) * -1
-    number*= -1
+else:
+    number *= -1
+    n = (number % 10) * -1
+    number *= -1
 if n == 0:
     print(f"Last digit of {number} is 0 and is 0")
 elif n > 5:
