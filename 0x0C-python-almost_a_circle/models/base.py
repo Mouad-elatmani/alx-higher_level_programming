bcab83993cee26071716423d6fadeb660f9bf948
mouad elatmani <mouad.elatmani@edu.uiz.ac.ma>
2023-10-16 13:02:26 -0700

    json file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,3 +15,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """function that returns the JSON representation
+        of an object (string)"""
+        if list_dictionaries is None:
+            return "[]"
+        return (json.dumps(list_dictionaries))
+
+    def save_to_file(cls, list_objs):
+        """function that writes an Object to a text file,
+        using a JSON representation"""
+        filename = cls.__name__ + ".json"
+        new_list = []
+        if list_objs:
+            for i in list_objs:
+                json_dict = cls.to_dictionary(i)
+                new_list.append(json_dict)
+        js_str = cls.to_json_string(new_list)
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(js_str)
