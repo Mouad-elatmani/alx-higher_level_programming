8009f29538f4330c4173afa22a0563317c3fd236
Mouad el atmani <mouad.elatmani@edu.uiz.ac.ma>
2023-11-15 14:38:28 -0800

    Task 0

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- a script that lists all the cities
 select cities.id,cities.name, states.name FROM cities
-JOIN states on cities.state_id = state.id ORDER BY cities.id
+JOIN states on cities.state_id = states.id ORDER BY cities.id
