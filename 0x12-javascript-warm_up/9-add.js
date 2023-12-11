cc05dd7e35930b297528e7ec6ab401e9b5941c87
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-11 13:54:06 -0800

    fix add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ function add (a, b) {
   console.log(parseInt(a) + parseInt(b));
 }
 if (!isNaN(process.argv[2]) && !isNaN(process.argv[3])) {
-  console.log(add(process.argv[2], process.argv[3]));
+  add(process.argv[2], process.argv[3]);
 } else {
   console.log('NaN');
 }
