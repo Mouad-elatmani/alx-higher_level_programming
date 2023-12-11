9089d2701ac4b9172568fe6ec064509e15a185dc
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-11 13:37:53 -0800

    Function add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function add (a, b) {
+  console.log(parseInt(a) + parseInt(b));
+}
+if (!isNaN(process.argv[2]) && !isNaN(process.argv[3])) {
+  console.log(add(process.argv[2], process.argv[3]));
+} else {
+  console.log('NaN');
+}
