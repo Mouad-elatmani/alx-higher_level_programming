91c43ce006747751b25e9b595f9603ee11ea361f
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-12 13:22:23 -0800

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && !isNaN(w) && h > 0 && !isNaN(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let result = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) { result += 'X'; }
+      if (i < this.height - 1) { result += '\n'; }
+    }
+    console.log(result);
+  }
+};
