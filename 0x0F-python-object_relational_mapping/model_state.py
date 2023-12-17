d73504ae150aa74e353ca7e5a149ce065c512190
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-17 08:24:23 -0800

    model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" import modules """
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """state class inherite fromBase Class"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True,autoincrement=True,nullable=False)
+    name = Column(String(128), nullable= False)
+
