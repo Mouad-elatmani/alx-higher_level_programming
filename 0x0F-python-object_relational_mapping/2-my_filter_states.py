19e2686f1495884951762be5d0e99080c49e9450
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-17 07:32:58 -0800

    fix filtre state task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ def main():
     conn = MySQLdb.connect(host="localhost", port=3306, user=usern,
                            passwd=pswd, db=dbname, charset="utf8")
     cur = conn.cursor()
-    temp = f"SELECT * FROM states WHERE name LIKE BINARY '{}'\
+    temp = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
             ORDER BY states.id ASC".format(search)
     cur.execute(temp)
     rows = cur.fetchall()
