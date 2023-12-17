304d80c0a7ac3d670b08529376685282db58d338
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-17 07:28:58 -0800

    fix filtre state task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,8 +4,8 @@ import MySQLdb
 
 
 def main():
-    usern, pswd, dbname, search = sys.argv[1], sys.argv[2], sys.argv[3],
-    sys.argv[4]
+    usern, pswd, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
+    search = sys.argv[4]
     conn = MySQLdb.connect(host="localhost", port=3306, user=usern,
                            passwd=pswd, db=dbname, charset="utf8")
     cur = conn.cursor()
