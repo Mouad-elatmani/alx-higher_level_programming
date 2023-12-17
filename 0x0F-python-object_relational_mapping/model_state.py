fe5b8ba61b2a29550a761a4b56633fde1ad2aff2
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-17 08:28:19 -0800

    fix style model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,11 +7,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """state class inherite fromBase Class"""
 
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True,autoincrement=True,nullable=False)
-    name = Column(String(128), nullable= False)
-
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
