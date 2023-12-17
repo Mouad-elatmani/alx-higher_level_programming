e6351bcafa5cd4f50a2ee557791779151c33985a
MOUAD EL ATMANI <mouad.elatmani@edu.uiz.ac.ma>
2023-12-17 07:47:40 -0800

    Safe filtre states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Imports modules"""
+import sys
+import MySQLdb
+
+
+def main():
+    usern, pswd, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
+    search = sys.argv[4]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=usern,
+                           passwd=pswd, db=dbname, charset="utf8")
+    cur = conn.cursor()
+    temp = "SELECT * FROM states WHERE name = %s \
+            ORDER BY states.id ASC"
+    cur.execute(temp, (search,))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
